92a0459357622ad79a2b7570268fa18a322701c4
Alessandro Marchesini <alessandromarchesini04@gmail.com>
2026-06-15 16:52:35 +0200

    Add Isolation Forest model metadata saving and update training process metrics

--- src/models/models.py
+++ src/models/models.py
@@ -1,14 +1,16 @@
 import joblib
-from datetime import datetime
+import numpy as np
+import pandas as pd
 
+from datetime import datetime
 from sklearn.ensemble import RandomForestClassifier, IsolationForest
-from sklearn.metrics import f1_score, precision_score, recall_score, confusion_matrix
+from sklearn.metrics import f1_score, precision_score, recall_score, confusion_matrix, roc_auc_score
 from sklearn.model_selection import train_test_split
 from utils.file_utils import append_data_to_csv
 from utils.paths import RF_MODELS_SAVED_DIR, RF_INFO_CSV, IF_MODELS_SAVED_DIR, IF_INFO_CSV
 
 
-def save_isolation_forest(model, params, training_dataset, dataset_type, samples, metrics):
+def save_isolation_forest(model, train_ratio, training_dataset, dataset_type, samples, metrics):
     print(f"Saving Isolation Forest model to: {IF_MODELS_SAVED_DIR}")
     existing_files = list(IF_MODELS_SAVED_DIR.glob('if_model_*.joblib'))
     id = len(existing_files) + 1
@@ -16,25 +18,27 @@ def save_isolation_forest(model, params, training_dataset, dataset_type, samples
 
     joblib.dump(model, IF_MODELS_SAVED_DIR / f'{model_name}.joblib')
 
+    params = model.get_params()
+
     results = {
         'timestamp': datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
         'model_name': model_name,
         'dataset_type': dataset_type,
         'training_dataset': training_dataset,
         'samples': samples,
-        'train_ratio': params['train_ratio'],
+        'train_ratio': train_ratio,
         
         # --- IPERPARAMETRI DI INPUT ---
         'n_estimators': params['n_estimators'],
         'max_samples': params['max_samples'],
         'max_features': params['max_features'],
         'contamination': params['contamination'],
-        'random_state': params.get('random_state', None), # Importante per la riproducibilità!
+        'random_state': params.get('random_state', None),
         
         # --- ATTRIBUTI APPRESI DAL MODELLO (Scikit-Learn) ---
-        'model_offset': float(iforest_model.offset_),
-        'max_samples_actual': int(iforest_model.max_samples_),
-        'n_features_in': int(iforest_model.n_features_in_),
+        'model_offset': float(model.offset_),
+        'max_samples_actual': int(model.max_samples_),
+        'n_features_in': int(model.n_features_in_),
         
         # --- METRICHE DI PERFORMANCE ---
         'tp': metrics['tp'],
@@ -44,26 +48,38 @@ def save_isolation_forest(model, params, training_dataset, dataset_type, samples
         'f1': metrics['f1'],
         'precision': metrics['precision'],
         'recall': metrics['recall'],
-        'auc_roc': metrics.get('auc_roc', None) # La metrica che abbiamo sistemato prima
+        'auc_roc': metrics.get('auc_roc', None)
     }
 
-
-    
+    append_data_to_csv(results, IF_INFO_CSV)
+    print(f"Model metadata saved to {IF_INFO_CSV.name}")
 
 
 def isolation_forest(data, dataset_type, training_dataset, train_ratio=0.8):
     print(f"Training Isolation Forest...")
 
-    X = data.drop(columns=["label", "attack_cat"])
-    y = data["label"]
-    X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=train_ratio, random_state=42)
+    data_normal = data[data['label'] == 0]
+    data_anomaly = data[data['label'] == 1]
+
+    train_normal, test_normal = train_test_split(data_normal, train_size=train_ratio, random_state=42)
+
+    n_anomaly_sample = int(len(test_normal) * 0.10)
+    test_anomaly = data_anomaly.sample(n=n_anomaly_sample, random_state=42)
+
+    data_test = pd.concat([test_normal, test_anomaly])
+
+    X_train = train_normal.drop(columns=['attack_cat', 'label']).values
+    X_test = data_test.drop(columns=['attack_cat', 'label']).values
+    y_test = data_test['label'].values
 
     model = IsolationForest(random_state=42, verbose=3)
     model.fit(X_train)
     y_pred = model.predict(X_test)
 
-    params = model.get_params()
-    params['train_ratio'] = train_ratio
+    y_pred = np.where(y_pred == 1, 0, 1)
+    
+    y_scores = -model.decision_function(X_test)
+    auc_roc = roc_auc_score(y_test, y_scores)
 
     f1 = f1_score(y_test, y_pred)
     precision = precision_score(y_test, y_pred)
@@ -79,10 +95,11 @@ def isolation_forest(data, dataset_type, training_dataset, train_ratio=0.8):
         'fn': fn,
         'f1': f1,
         'precision': precision,
-        'recall': recall
+        'recall': recall,
+        'auc_roc': auc_roc
     }
 
-    save_random_forest(model, params, training_dataset, dataset_type, X_train.shape[0], metrics)
+    save_isolation_forest(model, train_ratio, training_dataset, dataset_type, X_train.shape[0], metrics)
     print("Training process done.")
 
 
